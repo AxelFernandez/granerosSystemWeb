4395add84e00fde3ecbaee2aef6c612cb9eac892
Axel Fernandez <axelfernandez@eventbrite.com>
2020-05-03 11:55:55 -0300

    close you eyes here!

--- application/config/database.php
+++ application/config/database.php
@@ -75,11 +75,12 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'password',
-	'database' => 'Graneros',
-	'dbdriver' => 'mysqli',
+	'hostname' => 'ec2-52-7-39-178.compute-1.amazonaws.com',
+	'username' => 'ztgtckfkbbssbt',
+	'password' => '32873a871b4f4fae7ff6e1a960795b8f0d33214f8d80f074162223bd9bc8cdf3',
+	'database' => 'd2difdir7f2psd',
+	'dbdriver' => 'postgre',
+	'port'   => 5432,
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
